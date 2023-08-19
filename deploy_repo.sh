0f679f70950f461d6ae4e9c5f6051bf53c39665a
remiscarlet <remilia@scarl.et>
2023-08-19 21:28:21 +0000

    Add success checks and logging

--- deploy_repo.sh
+++ deploy_repo.sh
@@ -44,44 +44,60 @@ function log {
   echo ">> \$@"
 }
 
+function check_success {
+  if [[ x"\$?" != x"0" ]]; then
+    log "Failed '\$1'! Aborting"
+    exit 1
+  fi
+}
+
 pwd
 log "Deploying ${ORG_NAME_TO_DEPLOY}/${REPO_NAME_TO_DEPLOY}"
 
 log "Ensuring github.com is in known_hosts"
 grep "github.com" .ssh/known_hosts
-if [[ "$?" != "0" ]]; then
+if [[ "\$?" != "0" ]]; then
     ssh-keyscan github.com >> .ssh/known_hosts
+    check_success "ssh-keyscan"
 fi
 
 log "Ensuring org directory exists"
 mkdir -p $ORG_NAME_TO_DEPLOY
+check_success "mkdir"
 cd $ORG_NAME_TO_DEPLOY
+check_success "cd'ing"
 
 if [[ ! -d "$REPO_NAME_TO_DEPLOY" ]]; then
     log "Detected first time deployment - cloning repo"
     # First time deployment
     git clone git@github.com:${ORG_NAME_TO_DEPLOY}/${REPO_NAME_TO_DEPLOY}.git
+    check_success "git clone"
     cd $REPO_NAME_TO_DEPLOY
 
     log "Initializing git submodules"
     git submodule init
+    check_success "submodule init"
 
     log "Updating git submodules"
     git submodule update
+    check_success "submodule update"
 
 else
     log "Repo already exists - cd'ing in"
     # Repo already exists, just pull
     cd $REPO_NAME_TO_DEPLOY
+    check_success "cd'ing"
 
     log "Blowing away local changes"
     # Do we want to assume and blow away any local changes every time? Probably?
     git reset HEAD --hard
+    check_success "git reset"
 
     log "Git rebase pulling"
     # We shouldn't need to rebase if there aren't any local changes but uh... I guess to
     # account for potential "quick fix" local commits?
     git pull --rebase
+    check_success "git pull"
 fi
 
 log "Done"
